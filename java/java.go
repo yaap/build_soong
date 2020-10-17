0752b76104d8b44171df1b11bfd216aef0b6c65a
kubersharma001 <kubersharma001@gmail.com>
2020-10-17 11:37:36 +0200

    soong: java: ApexName is renamed to ApexVariationName
    It was renamed to ApexVariationName in preparation for reusing the same
    variation for multiple apexes, rename ApexName to ApexVariationName.
    
    fixes:
    build/soong/java/java.go:1686:7: j.ApexName undefined (type *Module has no field or method ApexName)
    
    Change-Id: I9c5c6999bdc6592f6d6535415464cf5276af6bf6

--- java/java.go
+++ java/java.go
@@ -1683,6 +1683,9 @@ func (j *Module) compile(ctx android.ModuleContext, aaptSrcJar android.Path) {
 		j.linter.compileSdkVersion = lintSDKVersionString(j.sdkVersion())
 		j.linter.javaLanguageLevel = flags.javaVersion.String()
 		j.linter.kotlinLanguageLevel = "1.3"
+		if j.ApexVariationName() != "" && ctx.Config().UnbundledBuild() {
+			j.linter.buildModuleReportZip = true
+		}
 		j.linter.lint(ctx)
 	}
 
