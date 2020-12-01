472366325683e8e99450525f8319b530f872b700
Adhitya Mohan <me@adhityamohan.in>
2020-12-01 21:43:19 +0530

    Revert "build: Soong: allow brotli to be used"
    This reverts commit 54b0d5f1a1dde5c170400cfd654cb5e600968cbc.

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -95,9 +95,7 @@ var Configuration = map[string]PathConfig{
 	"tr":      Allowed,
 	"unzip":   Allowed,
 	"zip":     Allowed,
-	"brotli":  Allowed,
-	"nproc":  Allowed,
-
+	"nproc":   Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
