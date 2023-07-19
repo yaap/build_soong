5dd5cd00c4b561bc3a633684c9e6cdd6a37f43fa
John Galt <johngaltfirstrun@gmail.com>
2023-07-19 10:08:47 -0400

    cc/config: bump to clang r498229 (17.0.3)

--- cc/config/global.go
+++ cc/config/global.go
@@ -255,6 +255,7 @@ var (
 		"-Wno-error=thread-safety-analysis",
 		"-Wno-error=unguarded-availability",
 		"-Wno-error=logical-op-parentheses",
+		"-Wno-error=shadow-uncaptured-local",
 	}
 
 	noOverrideExternalGlobalCflags = []string{
@@ -313,8 +314,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r487747c"
-	ClangDefaultShortVersion = "17.0.2"
+	ClangDefaultVersion      = "clang-r498229"
+	ClangDefaultShortVersion = "17.0.3"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
