111d5832f6b945e51b7fa227cd2217b60f90257d
AdityaK <appujee@google.com>
2023-07-31 17:13:31 -0700

    Enable hot cold split
    Change-Id: I6752ee000bc72c488ab197f6671514c9e5faa2bd

--- cc/config/global.go
+++ cc/config/global.go
@@ -176,6 +176,9 @@ var (
 
 		// Emit additional debug info for AutoFDO
 		"-fdebug-info-for-profiling",
+
+		// Enable Hot Cold splitting optimization.
+		"-mllvm -hot-cold-split=true",
 	}
 
 	commonGlobalLldflags = []string{
