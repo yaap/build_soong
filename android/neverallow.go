53b37efd9cd5dbab432f1bae418f151901ceef10
Michael Bestas <mkbestas@lineageos.org>
2025-03-09 11:17:50 +0200

    Revert "Add vendor/yaap to kotlin plugins neverallow"
    This reverts commit 681549f2a09613ab33abe57d7ee851066c53c324.
    
    Reason for revert: Added in AOSP
    
    Change-Id: I95f9631246950ad55df4c6f6f4a0af566b4ddde3

--- android/neverallow.go
+++ android/neverallow.go
@@ -326,7 +326,6 @@ func createKotlinPluginRule() []Rule {
 		// Actual path prebuilts/sdk/current/androidx/m2repository/androidx/compose/compiler/compiler-hosted
 		"prebuilts/sdk/current/androidx",
 		"external/kotlinc",
-		"vendor/yaap/kotlin",
 	}
 
 	return []Rule{
